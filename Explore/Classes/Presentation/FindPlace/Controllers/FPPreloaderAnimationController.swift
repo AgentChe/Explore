bd9ed3464f6f05f9f75b0e7ee76690543ea54382
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2020-11-05 16:56:22 +0300

    change app icon

---
+++ Explore/Classes/Presentation/FindPlace/Controllers/FPPreloaderAnimationController.swift
@@ -0,0 +1,9 @@
+//
+//  FPPreloaderAnimationController.swift
+//  Explore
+//
+//  Created by Andrey Chernyshev on 05.11.2020.
+//  Copyright © 2020 Andrey Chernyshev. All rights reserved.
+//
+
+import Foundation
